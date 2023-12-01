e4c6c6cfbfff56816e298ecd2a6fc8d853aeb816
Nitin Pawar <nitin.pawar150@gmail.com>
2023-12-01 14:56:58 +0530

    commit

--- minimal_dash.py
+++ minimal_dash.py
@@ -2,9 +2,10 @@ from dash import Dash, html, dcc, callback, Output, Input, dash_table
 import plotly.express as px
 import pandas as pd
 
-df = pd.read_csv("C:/Users/nitin/dash/df.csv")
+df = pd.read_csv("df.csv")
 
 app = Dash(__name__)
+
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
