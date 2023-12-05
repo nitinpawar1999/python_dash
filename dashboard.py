39a7ca4af086ad11af4af14dedb06476de8bb7f6
Nitin Pawar <nitin.pawar150@gmail.com>
2023-12-05 23:41:50 +0530

    commit

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,8 @@
 import dash
 import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html, dash_table
-import plotly.express as px
 import pandas as pd
+import plotly.graph_objs as go
 
 df = pd.read_csv("df.csv")
 
@@ -22,46 +22,60 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
 SIDEBAR_STYLE = {
-    "position": "fixed",
-    "top": 0,
-    "left": 0,
-    "bottom": 0,
     "width": "16rem",
-    "padding": "4rem 2rem",
+    "padding": "1rem 2rem",
     "background-color": "#f8f9fa",
 }
 
 # the styles for the main content position it to the right of the sidebar and
 # add some padding.
 CONTENT_STYLE = {
-    "margin-left": "18rem",
+    "margin-left": "1rem",
     "margin-right": "2rem",
-    "padding": "2rem 1rem",
+    "padding": "1rem",
 }
 
-sidebar_list = [html.H2("DashBoard For Something", className="display-6"),
+DROPDOWN_WRAPPER = {
+    "margin-right": "2rem",
+    "padding": "1rem",
+}
+
+BODY_LAYOUT = {
+    "height": "120vh",
+    "display": "flex",
+}
+
+TABLE_CHART_STYLE = {
+    "display": "flex",
+    "margin-left": "1rem",
+    "margin-right": "2rem",
+    "padding": "1rem",
+}
+
+sidebar_list = [html.H2("Dashboard", className="display-6"),
                 html.Hr(),
                 html.Div([
-                    html.P('BS'),
+                    html.P('Var1', style={'margin-bottom': 0}),
                     dcc.Dropdown(
                         id='dynamic-dropdown-container1',
                         options=list(data_dict.keys()),
-                        value='',
+                        value=list(data_dict.keys())[0],
                         multi=False,
                         clearable=True,
                         disabled=False,)],
-                style={'display': 'block'}),
-                html.Hr(),
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
                 html.Div([
-                    html.P('Location'),
+                    html.P('Var2', style={'margin-bottom': 0}),
                     dcc.Dropdown(
                         id='dynamic-dropdown-container2',
-                        options=[],
-                        value='',
+                        options=sorted(
+                            list(data_dict[list(data_dict.keys())[0]])),
+                        value=sorted(
+                            list(data_dict[list(data_dict.keys())[0]]))[0],
                         multi=False,
                         clearable=True,
                         disabled=False,)],
-                style={'display': 'block'})]
+                style={'display': 'block', 'margin-bottom': '0.75rem'})]
 
 
 sidebar = html.Div(
@@ -86,18 +100,18 @@ def update_dynamic_dropdown_container2(value):
     return []
 
 
-content = html.H3(id="output", style=CONTENT_STYLE)
+heading = html.H3(id="output", style=CONTENT_STYLE)
 
 
 @app.callback(Output("output", "children"), [Input("dynamic-dropdown-container1", "value"), Input("dynamic-dropdown-container2", "value")])
 def display_value(var1, var2):
     if var1 != "" and var2 != "" and var1 != None and var2 != None:
-        return f"Selected value: BS -> {var1} | Location -> {var2}"
+        return "Factors"
     return
 
 
 dropdown_selections = html.Div(
-    id="dropdown_selections_output", style=CONTENT_STYLE)
+    id="dropdown_selections_output", style=DROPDOWN_WRAPPER)
 
 
 @app.callback(Output("dropdown_selections_output", "children"), [Input("dynamic-dropdown-container1", "value"), Input("dynamic-dropdown-container2", "value")])
@@ -105,43 +119,43 @@ def display_value(var1, var2):
     if var1 != "" and var2 != "" and var1 != None and var2 != None:
 
         filtered_df = df[(df['Var1'] == var1) & (df['Var2'] == var2)]
-        wps_list = filtered_df['var3'].drop_duplicates().to_list()
-        lateral_list = filtered_df['var4'].drop_duplicates().to_list()
-        quantile_list = filtered_df['var5'].drop_duplicates().to_list()
+        var3_list = filtered_df['var3'].drop_duplicates().to_list()
+        var4_list = filtered_df['var4'].drop_duplicates().to_list()
+        var5_list = filtered_df['var5'].drop_duplicates().to_list()
 
         dropdown_selections_list = [
             html.Div([
-                html.P('WPS'),
+                html.P('Var3', style={'margin-bottom': 0}),
                 dcc.Dropdown(
                     id='dynamic-dropdown-container3',
-                    options=wps_list,
-                    value='',
+                    options=var3_list,
+                    value=[var3_list[0]],
                     multi=True,
                     clearable=True,
                     disabled=False,)],
-                style={'display': 'block'}),
-            html.Hr(),
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
+
             html.Div([
-                html.P('Lateral'),
+                html.P('Var4', style={'margin-bottom': 0}),
                 dcc.Dropdown(
                     id='dynamic-dropdown-container4',
-                    options=lateral_list,
-                    value='',
+                    options=var4_list,
+                    value=[var4_list[0]],
                     multi=True,
                     clearable=True,
                     disabled=False,)],
-                style={'display': 'block'}),
-            html.Hr(),
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
+
             html.Div([
-                html.P('Quantile'),
+                html.P('Var5', style={'margin-bottom': 0}),
                 dcc.Dropdown(
                     id='dynamic-dropdown-container5',
-                    options=quantile_list,
-                    value='',
+                    options=var5_list,
+                    value=[var5_list[0]],
                     multi=True,
                     clearable=True,
                     disabled=False,)],
-                style={'display': 'block'})]
+                style={'display': 'block', 'margin-bottom': '0.75rem'})]
 
         return html.Div(dropdown_selections_list, style=CONTENT_STYLE,)
     return
@@ -161,22 +175,33 @@ def display_value(var1, var2, var3, var4, var5):
         tmp_df = df[(df['Var1'] == var1) & (df['Var2'] == var2) & (
             df['var3'].isin(var3)) & (df['var4'].isin(var4)) & (df['var5'].isin(var5))]
         tmp_df_1 = tmp_df[['var3', 'var4', 'var5', 'Z']]
+        fig = go.Figure()
+        for i in range(len(tmp_df)):
+            fig = fig.add_trace(go.Scatter(
+                x=list(tmp_df['t'].iloc[i]), y=list(tmp_df['y'].iloc[i])))
         return html.Div(
             [html.Div(
-                [dcc.Graph(figure=px.scatter(tmp_df[['y', 't']], x='y', y='t'), id='graph-placeholder')]),
+                [dcc.Graph(figure=fig, id='graph-placeholder')]),
              html.Div(
                 [dash_table.DataTable(
-                    data=tmp_df_1.to_dict('records'), page_size=10),
+                    data=tmp_df_1.to_dict('records'), page_size=10,
+                    style_header={
+                        'backgroundColor': '#f8f9fa',
+                        'fontWeight': 'bold'
+                    },),
                     html.Hr(),
-                    html.P(f"mean of EUR - {tmp_df_1['Z'].mean()}"),
-                    html.P(f"Std of EUR - {tmp_df_1['Z'].std()}")]),],
-            style=CONTENT_STYLE)
+                    html.P(f"mean of Z - {tmp_df_1['Z'].mean()}"),
+                    html.P(f"Std of Z - {tmp_df_1['Z'].std()}")],
+                style={"margin-top": "6.4rem"}),],
+            style=TABLE_CHART_STYLE)
     return
 
 
-app.layout = html.Div(
-    [dcc.Location(id="url"), sidebar, content, dropdown_selections, content_table])
+content = html.Div([heading, dropdown_selections, content_table], style={
+                   "flex-basis": "100%"})
 
+app.layout = html.Div(
+    [dcc.Location(id="url"), sidebar, content], style=BODY_LAYOUT)
 
 if __name__ == "__main__":
     app.run_server(port=8080)
---
+++ dashboard_v2.py
@@ -0,0 +1,187 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import Input, Output, dcc, html, dash_table
+import pandas as pd
+import plotly.graph_objs as go
+
+df = pd.read_csv("df.csv")
+
+temp_df = df[['Var1', 'Var2']]
+temp_df = temp_df.drop_duplicates()
+data_dict = {}
+
+for ele in temp_df.values.tolist():
+    if ele[0] in data_dict:
+        data_dict[ele[0]].add(ele[1])
+    else:
+        data_dict[ele[0]] = set()
+        data_dict[ele[0]].add(ele[1])
+
+radioItems_lst = []
+
+for key in data_dict:
+    for value in data_dict[key]:
+        radioItems_lst.append(
+            {"label": f"{key} - {value}", "value": f"{key}|&|{value}"})
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "width": "16rem",
+    "padding": "1rem 2rem",
+    "background-color": "#f8f9fa",
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "1rem",
+    "margin-right": "2rem",
+    "padding": "1rem",
+}
+
+DROPDOWN_WRAPPER = {
+    "margin-right": "2rem",
+    "padding": "1rem",
+}
+
+BODY_LAYOUT = {
+    "height": "120vh",
+    "display": "flex",
+}
+
+TABLE_CHART_STYLE = {
+    "display": "flex",
+    "margin-left": "1rem",
+    "margin-right": "2rem",
+    "padding": "1rem",
+}
+
+sidebar_list = [html.H2("Dashboard", className="display-6"),
+                html.Hr(),
+                html.Div([
+                    html.P('radioBtn', style={'margin-bottom': 0}),
+                    dbc.RadioItems(
+                        id="radios",
+                        className="btn-group",
+                        inputClassName="btn-check",
+                        labelClassName="btn btn-outline-primary",
+                        labelCheckedClassName="active",
+                        options=radioItems_lst,
+                        value=radioItems_lst[0]["value"],)],
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
+                ]
+
+
+sidebar = html.Div(
+    sidebar_list,
+    style=SIDEBAR_STYLE,
+)
+
+
+heading = html.H3(id="output", style=CONTENT_STYLE)
+
+
+@app.callback(Output("output", "children"), [Input("radios", "value")])
+def display_value(val):
+    if val != "" and val != None:
+        return "Factors"
+    return
+
+
+dropdown_selections = html.Div(
+    id="dropdown_selections_output", style=DROPDOWN_WRAPPER)
+
+
+@app.callback(Output("dropdown_selections_output", "children"), [Input("radios", "value")])
+def display_value(val:str):
+    if val != "" and val != None:
+
+        filtered_df = df[(df['Var1'] == val.split('|&|')[0]) & (df['Var2'] == val.split('|&|')[1])]
+        var3_list = filtered_df['var3'].drop_duplicates().to_list()
+        var4_list = filtered_df['var4'].drop_duplicates().to_list()
+        var5_list = filtered_df['var5'].drop_duplicates().to_list()
+
+        dropdown_selections_list = [
+            html.Div([
+                html.P('Var3', style={'margin-bottom': 0}),
+                dcc.Dropdown(
+                    id='dynamic-dropdown-container3',
+                    options=var3_list,
+                    value=[var3_list[0]],
+                    multi=True,
+                    clearable=True,
+                    disabled=False,)],
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
+
+            html.Div([
+                html.P('Var4', style={'margin-bottom': 0}),
+                dcc.Dropdown(
+                    id='dynamic-dropdown-container4',
+                    options=var4_list,
+                    value=[var4_list[0]],
+                    multi=True,
+                    clearable=True,
+                    disabled=False,)],
+                style={'display': 'block', 'margin-bottom': '0.75rem'}),
+
+            html.Div([
+                html.P('Var5', style={'margin-bottom': 0}),
+                dcc.Dropdown(
+                    id='dynamic-dropdown-container5',
+                    options=var5_list,
+                    value=[var5_list[0]],
+                    multi=True,
+                    clearable=True,
+                    disabled=False,)],
+                style={'display': 'block', 'margin-bottom': '0.75rem'})]
+
+        return html.Div(dropdown_selections_list, style=CONTENT_STYLE,)
+    return
+
+
+content_table = html.Div(id="output_table", style=CONTENT_STYLE)
+
+
+@app.callback(Output("output_table", "children"),
+              [Input("radios", "value"),
+               Input("dynamic-dropdown-container3", "value"),
+               Input("dynamic-dropdown-container4", "value"),
+               Input("dynamic-dropdown-container5", "value")])
+def display_value(val, var3, var4, var5):
+    if val.split('|&|')[0] != "" and val.split('|&|')[1] != "" and var3 != "" and var4 != "" and var5 != "" and val.split('|&|')[0] != None and val.split('|&|')[1] != None and var3 != None and var4 != None and var5 != None:
+        tmp_df = df[(df['Var1'] == val.split('|&|')[0]) & (df['Var2'] == val.split('|&|')[1]) & (
+            df['var3'].isin(var3)) & (df['var4'].isin(var4)) & (df['var5'].isin(var5))]
+        tmp_df_1 = tmp_df[['var3', 'var4', 'var5', 'Z']]
+        fig = go.Figure()
+        for i in range(len(tmp_df)):
+            fig = fig.add_trace(go.Scatter(
+                x=list(tmp_df['t'].iloc[i]), y=list(tmp_df['y'].iloc[i])))
+        return html.Div(
+            [html.Div(
+                [dcc.Graph(figure=fig, id='graph-placeholder')]),
+             html.Div(
+                [dash_table.DataTable(
+                    data=tmp_df_1.to_dict('records'), page_size=10,
+                    style_header={
+                        'backgroundColor': '#f8f9fa',
+                        'fontWeight': 'bold'
+                    },),
+                    html.Hr(),
+                    html.P(f"mean of Z - {tmp_df_1['Z'].mean()}"),
+                    html.P(f"Std of Z - {tmp_df_1['Z'].std()}")],
+                style={"margin-top": "6.4rem"}),],
+            style=TABLE_CHART_STYLE)
+    return
+
+
+content = html.Div([heading, dropdown_selections, content_table], style={
+                   "flex-basis": "100%"})
+
+app.layout = html.Div(
+    [dcc.Location(id="url"), sidebar, content], style=BODY_LAYOUT)
+
+if __name__ == "__main__":
+    app.run_server(port=8080)
